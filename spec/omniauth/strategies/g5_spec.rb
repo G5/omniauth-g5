de5e35afed73770b1cdffe8de191394d8d8eddf2
maeve <maeve.revels@getg5.com>
2013-06-09 04:12:14 +0000

    Flesh out specs around client option configuration

--- spec/omniauth/strategies/g5_spec.rb
+++ spec/omniauth/strategies/g5_spec.rb
@@ -1,22 +1,43 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::G5 do
+  before { OmniAuth.config.test_mode = true }
+  after { OmniAuth.config.test_mode = false }
+
   subject { strategy }
-  let(:strategy) { OmniAuth::Strategies::G5.new({}) }
+  let(:strategy) { OmniAuth::Strategies::G5.new(rack_app, app_id, app_secret, options) }
+
+  let(:rack_app) { mock(:rack_app) }
+  let(:app_id) { 'test_app' }
+  let(:app_secret) { '1234567890qwerty' }
+  let(:options) { Hash.new }
 
   its(:name) { should == :g5 }
+
+  it 'should have the correct client id' do
+    expect(strategy.options[:client_id]).to eq(app_id)
+  end
+
+  it 'should have the correct client secret' do
+    expect(strategy.options[:client_secret]).to eq(app_secret)
+  end
 
   describe 'client_options' do
     subject(:client_options) { strategy.options.client_options }
 
-    its(:site) { should == 'https://auth.g5search.com' }
-    its(:authorize_url) { should == '/oauth/authorize' }
-  end
+    context 'with default options' do
+      its(:site) { should == 'https://auth.g5search.com' }
+      its(:authorize_url) { should == '/oauth/authorize' }
+    end
 
-  describe '#uid' do
-  end
+    context 'with partially overridden options' do
+      let(:options) do
+        {:client_options => {:site => 'https://custom.app.com'}}
+      end
 
-  describe '#info' do
+      its(:site) { should == 'https://custom.app.com' }
+      its(:authorize_url) { should == '/oauth/authorize' }
+    end
   end
 
   describe '#raw_info' do
