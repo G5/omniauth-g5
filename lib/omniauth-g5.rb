b74126706e7e09c0a921db7f7099f82c653cd427
maeve <maeve.revels@getg5.com>
2014-03-07 01:29:52 +0000

    Move user exporter to devise_g5_authenticatable

--- lib/omniauth-g5.rb
+++ lib/omniauth-g5.rb
@@ -1,7 +1,2 @@
-require 'omniauth-oauth2'
-
 require 'omniauth-g5/version'
 require 'omniauth/strategies/g5'
-
-# TODO: eventually, this should go into the devise extension
-require 'g5/user_exporter'
